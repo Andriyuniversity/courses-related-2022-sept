16b06912530133bd99b8bf47a26c52c7956225a6
Andriyuniversity <andriyuniversity@gmail.com>
2023-01-20 18:38:55 +0200

    Task1 done

--- node-js/homework1/app/src/App.js
+++ node-js/homework1/app/src/App.js
@@ -2,24 +2,9 @@ import React from 'react'
 import './App.css';
 import Simpsons from "./components/simpson-base/Simpsons/Simpsons";
 
-function App() {
   return (
       < Simpsons />
   );
 }
 
 export default App;
-
-
-// import { ImageBlock } from "./components/ImageBlock/ImageBlock";
-
-// const App = () => {
-//     const url = 'https://www.seekpng.com/png/detail/69-690313_bart-simpsons-png-bart-simpson-ass.png'
-//     return (
-//         <div className="App">
-//             <div className="App">
-//                 <ImageBlock url={url} bool={false}/>
-//             </div>
-//         </div>
-//     );
-// }
