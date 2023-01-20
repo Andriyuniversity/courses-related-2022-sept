d3c8a5240a54d89c1f870f42cc9ada181a95b99f
Andriyuniversity <andriyuniversity@gmail.com>
2023-01-20 18:39:51 +0200

    task 1 done

--- node-js/homework1/app/src/App.js
+++ node-js/homework1/app/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import './App.css';
-import Simpsons from "./components/simpson-base/Simpsons/Simpsons";
+import Simpsons from "./components/Simpsons/Simpsons";
+
+const App = () => {
+  let task = (number) => React.createElement("div", {className: "task", id: `task${number}`, key: `task${number}`}, [<h1>Task {number}</h1>])
 
   return (
+    
+    [
+      task(1),
       < Simpsons />
+    ]
+    
   );
 }
 
---
+++ node-js/homework1/app/src/components/Simpsons/Simpsons.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Simpson from '../simpson-base/simpson-base'
+import './Simpsons.css'
+
+const Simpsons = () => {
+    let name = 'Bart';
+    let image = 'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png';
+    let text='Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+        ' Architecto beatae nam voluptatum. Nostrum saepe, ut.'
+    let simpson;
+    let simpsonsArray = []
+
+
+    for (let i = 0; i < 6; i++) {
+        let simpson = 'simpson' + `${i}`
+        console.log(simpson)
+        simpsonsArray.push({
+            simpson:
+            <div className="simpson_base" key={`${i}`}>
+                <Simpson name={name + `${i}`} image={image} about={text} />
+            </div>
+})
+    }
+        return (
+            <div className='simpsons_base'>
+                {simpsonsArray.map((value) => value["simpson"])}
+            </div>
+
+    )
+}
+export default Simpsons
